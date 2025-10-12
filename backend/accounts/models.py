f1d544082c85c10faf374af324e4f1e51731b682
Levah <emmanuelnater@gmail.com>
2025-10-12 13:19:30 +0100

    first commit working

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -6,10 +6,42 @@ from django.core.validators import RegexValidator
 class UserManager(BaseUserManager):
     """Custom user manager for phone number authentication."""
     
+    def normalize_phone_number(self, phone_number):
+        """
+        Normalize phone number to international format.
+        Converts '08167791934' to '+2348167791934'
+        """
+        if not phone_number:
+            return phone_number
+        
+        # Remove any spaces, dashes, or parentheses
+        phone_number = ''.join(filter(str.isdigit, phone_number.replace('+', '')))
+        
+        # If starts with 0 and has 11 digits (Nigerian format), convert to +234
+        if phone_number.startswith('0') and len(phone_number) == 11:
+            phone_number = '+234' + phone_number[1:]
+        # If doesn't start with +, assume it needs +234
+        elif not phone_number.startswith('+'):
+            # If it's 10 digits, add +234
+            if len(phone_number) == 10:
+                phone_number = '+234' + phone_number
+            # If it's 13 digits starting with 234, add +
+            elif len(phone_number) == 13 and phone_number.startswith('234'):
+                phone_number = '+' + phone_number
+            else:
+                phone_number = '+' + phone_number
+        else:
+            phone_number = '+' + phone_number
+        
+        return phone_number
+    
     def create_user(self, phone_number, password=None, **extra_fields):
         """Create and return a regular user with a phone number."""
         if not phone_number:
             raise ValueError('The Phone Number field must be set')
+        
+        # Normalize the phone number
+        phone_number = self.normalize_phone_number(phone_number)
         
         user = self.model(phone_number=phone_number, **extra_fields)
         if password:
@@ -29,6 +61,14 @@ class UserManager(BaseUserManager):
             raise ValueError('Superuser must have is_superuser=True.')
         
         return self.create_user(phone_number, password, **extra_fields)
+    
+    def get_by_natural_key(self, phone_number):
+        """
+        Override to normalize phone number during authentication.
+        This allows login with either 08167791934 or +2348167791934
+        """
+        phone_number = self.normalize_phone_number(phone_number)
+        return self.get(**{self.model.USERNAME_FIELD: phone_number})
 
 
 class User(AbstractUser):
@@ -58,6 +98,12 @@ class User(AbstractUser):
     
     # Assign the custom manager
     objects = UserManager()
+    
+    def save(self, *args, **kwargs):
+        """Override save to normalize phone number before saving."""
+        if self.phone_number:
+            self.phone_number = User.objects.normalize_phone_number(self.phone_number)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.phone_number}"
@@ -72,6 +118,12 @@ class OTPVerification(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+    
+    def save(self, *args, **kwargs):
+        """Normalize phone number before saving."""
+        if self.phone_number:
+            self.phone_number = User.objects.normalize_phone_number(self.phone_number)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"OTP for {self.phone_number}"
